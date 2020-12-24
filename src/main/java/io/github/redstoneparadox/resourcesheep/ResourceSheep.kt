c668036b1b4c2d2c4ad4c8c5e4cd446b232bfab5
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-23 16:19:35 -0800

    Initial setup

---
+++ src/main/java/io/github/redstoneparadox/resourcesheep/ResourceSheep.kt
@@ -0,0 +1,9 @@
+package io.github.redstoneparadox.resourcesheep
+
+import net.fabricmc.api.ModInitializer
+
+object ResourceSheep: ModInitializer {
+    override fun onInitialize() {
+        println("Sanity check!")
+    }
+}
