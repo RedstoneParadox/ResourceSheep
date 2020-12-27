b43c16945cf361c9d4e5bf34bacb3ee68b5ff10b
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-27 12:57:32 -0800

    Alright, you just can't place the wool

--- src/main/java/io/github/redstoneparadox/resourcesheep/item/ResourceWoolItem.kt
+++ src/main/java/io/github/redstoneparadox/resourcesheep/item/ResourceWoolItem.kt
@@ -4,12 +4,13 @@ import io.github.redstoneparadox.resourcesheep.config.SheepResourceLoader
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.block.Blocks
 import net.minecraft.item.BlockItem
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.text.Text
 import net.minecraft.text.TranslatableText
 import net.minecraft.util.Identifier
 
-class ResourceWoolItem: BlockItem(Blocks.AIR, FabricItemSettings()) {
+class ResourceWoolItem: Item(FabricItemSettings()) {
     override fun getName(stack: ItemStack): Text {
         val nbt = stack.orCreateTag
         val resourceId = Identifier.tryParse(nbt.getString("resource"))
