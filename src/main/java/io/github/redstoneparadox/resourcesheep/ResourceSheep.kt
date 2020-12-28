011fb10dd0ede7f6bbca5176b2f8b9f4263a0fe7
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-27 18:15:35 -0800

    start work on recipe template system

--- src/main/java/io/github/redstoneparadox/resourcesheep/ResourceSheep.kt
+++ src/main/java/io/github/redstoneparadox/resourcesheep/ResourceSheep.kt
@@ -1,16 +1,12 @@
 package io.github.redstoneparadox.resourcesheep
 
+import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
+import io.github.redstoneparadox.resourcesheep.config.SheepResourceLoader
 import io.github.redstoneparadox.resourcesheep.entity.ResourceSheepEntity
 import io.github.redstoneparadox.resourcesheep.item.ResourceSheepItems
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricDefaultAttributeRegistry
-import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilder
-import net.minecraft.entity.EntityDimensions
-import net.minecraft.entity.EntityType
-import net.minecraft.entity.SpawnGroup
-import net.minecraft.entity.passive.SheepEntity
 import net.minecraft.util.Identifier
-import net.minecraft.util.registry.Registry
 
 object ResourceSheep: ModInitializer {
     val MOD_ID: String = "resourcesheep"
@@ -18,6 +14,37 @@ object ResourceSheep: ModInitializer {
     override fun onInitialize() {
         ResourceSheepEntity.init()
         ResourceSheepItems.init()
+    }
+
+    private fun fillTemplates() {
+        for (resource in SheepResourceLoader.getResources()) {
+            // TODO: Actually load the recipe template
+            val json = JsonObject()
+
+            if (json.has("key")) {
+                val key = json["key"] as JsonObject
+
+                for (entry in key.entrySet()) {
+                    val value = entry.value
+
+                    if (value is JsonObject && value["item"].asString == "sheepresources:resource_wool") {
+                        val data = JsonObject()
+                        val require = JsonObject()
+                        require.add("resource", JsonPrimitive(resource.id.toString()))
+                        data.add("require", require)
+                    }
+                }
+            }
+
+            val result = json["result"]
+
+            if (result is JsonObject) {
+                result.add("item", JsonPrimitive(resource.id.toString()))
+            }
+            else {
+                json.add("result", JsonPrimitive(resource.id.toString()))
+            }
+        }
     }
 
     fun id(path: String): Identifier {
